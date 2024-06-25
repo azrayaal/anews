05af54d465b30ea8fe760a2cc6d7fd1da20d7b45
azrayaal <azrayazidalkautsar@gmail.com>
2024-06-25 08:50:46 +0700

    update

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 import dataCarouselContent from "../../assets/data_dummy/data-carousel-content";
 import Categories from "../components/categories";
 import Navbar from "../components/navbar";
 import CardContent from "../components/post";
 import Post from "../components/post/post";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default function Home() {
   return (
@@ -25,12 +27,47 @@ export default function Home() {
             />
           ))}
         </div>
-
-        <div className="my-4 p-4">
+        {/* Popular post */}
+        <div className="my-4 p-4 Popular-post">
           <div className="my-4">
-            <div className="flex">
+            <div className="flex pb-4">
               <div className="p-2 w-1 bg-red-500" />
               <h1 className="text-black pl-3">Popular Posts</h1>
+              <div className=" rounded-md bg-white ml-auto">
+                <Link to="/post">
+                  <p className="text-gray-500 my-1 mx-2 text-xs">Show all ▶️</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <Post />
+        </div>
+        {/* New Post  */}
+        <div className="my-4 p-4 new-post">
+          <div className="my-4">
+            <div className="flex pb-4">
+              <div className="p-2 w-1 bg-red-500" />
+              <h1 className="text-black pl-3">New Posts</h1>
+              <div className=" rounded-md bg-white ml-auto">
+                <Link to="/post">
+                  <p className="text-gray-500 my-1 mx-2 text-xs">Show all ▶️</p>
+                </Link>
+              </div>
+            </div>
+          </div>
+          <Post />
+        </div>
+        {/* Trendy Post */}
+        <div className="my-4 p-4 Trendy-post">
+          <div className="my-4">
+            <div className="flex pb-4">
+              <div className="p-2 w-1 bg-red-500" />
+              <h1 className="text-black pl-3">Trendy Posts</h1>
+              <div className=" rounded-md bg-white ml-auto">
+                <Link to="/post">
+                  <p className="text-gray-500 my-1 mx-2 text-xs">Show all ▶️</p>
+                </Link>
+              </div>
             </div>
           </div>
           <Post />
--- src/pages/components/post/post.tsx
+++ src/pages/components/post/post.tsx
@@ -1,31 +1,38 @@
 import Weather from "../../../assets/img/weather/01.jpg";
 import stark from "../../../assets/stark.png";
+import { FaRegBookmark } from "react-icons/fa6";
 
 export default function Post() {
   return (
     <>
       <div className="max-w-sm overflow-hidden shadow-lg rounded-xl bg-white text-black">
-        <img className="w-full" src={Weather} alt="Sunset in the mountains" />
-        <div className="px-6 py-4">
+        <div className="p-2">
+          <img className="w-full rounded-xl" src={Weather} alt="img" />
+        </div>
+        <div className="px-6 pt-4">
           <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
+          <p className="text-gray-700 text-base overflow-hidden overflow-ellipsis whitespace-nowrap w-30">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
             Voluptatibus quia, nulla! Maiores et perferendis eaque,
             exercitationem praesentium nihil.
           </p>
+          {/* <p className="text-gray-700 text-base">
+            {desc.length > 30 ? `${desc.substring(0, 30)}...` : desc}
+          </p> */}
         </div>
-        <div className="px-6 pt-6 ">
-          <span className="inline-block bg-gray-200 rounded-md px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 w-full p-6">
+        <div className="p-2">
+          <span className="inline-block bg-gray-200 rounded-md px-3 py-3 text-sm font-semibold text-gray-700 mr-2 w-full p-6">
             <div className="flex">
               <img
                 src={stark}
                 alt="profile"
                 className="w-10 h-10 rounded-md my-auto shadow-md mr-4"
               />
-              <div className="text">
-                <div>Azrayaal</div>
-                <div>June 24, 2024</div>
+              <div className="text w-full">
+                <p className="font-bold pb-1">Azrayaal</p>
+                <p className="font-thin text-gray-500 text-xs">June 24, 2024</p>
               </div>
+              <FaRegBookmark className="m-auto text-lg" />
             </div>
           </span>
         </div>
